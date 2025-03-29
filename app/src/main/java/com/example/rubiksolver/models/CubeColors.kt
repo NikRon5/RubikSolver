679e5d325c2cf34acd656ba8a408a9bf307fe00f
Nick <ronilovo2@gmail.com>
2025-03-29 23:30:57 +0300

    fix(cube): better order of colors

--- app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
+++ app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
@@ -4,11 +4,11 @@ import android.graphics.Color
 
 object CubeColors {
     val centerColors = mapOf(
-        "front" to Color.parseColor("#009E60"),
-        "up" to Color.parseColor("#FFFFFF"),
+        "front" to Color.parseColor("#FFFFFF"),
+        "up" to Color.parseColor("#0051BA"),
         "left" to Color.parseColor("#FFA500"),
         "right" to Color.parseColor("#C41E3A"),
-        "back" to Color.parseColor("#0051BA"),
-        "down" to Color.parseColor("#FFD500")
+        "back" to Color.parseColor("#FFD500"),
+        "down" to Color.parseColor("#009E60")
     )
 }
