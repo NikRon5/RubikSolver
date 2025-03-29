127e99eb1d8e1b57a86790dd6d3ea8e22338766f
Nick <ronilovo2@gmail.com>
2025-03-29 23:22:03 +0300

    feat(cube): better keys for centerColors

--- app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
+++ app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
@@ -4,15 +4,11 @@ import android.graphics.Color
 
 object CubeColors {
     val centerColors = mapOf(
-        Face.FRONT to Color.parseColor("#C41E3A"),
-        Face.UP to Color.parseColor("#FFFFFF"),
-        Face.LEFT to Color.parseColor("#009E60"),
-        Face.RIGHT to Color.parseColor("#0051BA"),
-        Face.BACK to Color.parseColor("#FFA500"),
-        Face.DOWN to Color.parseColor("#FFD500")
+        "front" to Color.parseColor("#009E60"),
+        "up" to Color.parseColor("#FFFFFF"),
+        "left" to Color.parseColor("#FFA500"),
+        "right" to Color.parseColor("#C41E3A"),
+        "back" to Color.parseColor("#0051BA"),
+        "down" to Color.parseColor("#FFD500")
     )
-
-    enum class Face {
-        FRONT, UP, LEFT, RIGHT, BACK, DOWN
-    }
 }
