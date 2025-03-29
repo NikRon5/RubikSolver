920f484533f4c8c01a75a11f7fb33997e7d40d3e
Nick <ronilovo2@gmail.com>
2025-03-29 22:37:17 +0300

    fix(cube): better colors

--- app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
+++ app/src/main/java/com/example/rubiksolver/models/CubeColors.kt
@@ -4,12 +4,12 @@ import android.graphics.Color
 
 object CubeColors {
     val centerColors = mapOf(
-        Face.FRONT to Color.RED,
-        Face.UP to Color.WHITE,
-        Face.LEFT to Color.GREEN,
-        Face.RIGHT to Color.BLUE,
+        Face.FRONT to Color.parseColor("#C41E3A"),
+        Face.UP to Color.parseColor("#FFFFFF"),
+        Face.LEFT to Color.parseColor("#009E60"),
+        Face.RIGHT to Color.parseColor("#0051BA"),
         Face.BACK to Color.parseColor("#FFA500"),
-        Face.DOWN to Color.YELLOW
+        Face.DOWN to Color.parseColor("#FFD500")
     )
 
     enum class Face {
