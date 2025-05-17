59bd12aecdcd9668da0aa5960cca40bf582322ac
Nick <ronilovo2@gmail.com>
2025-05-17 16:22:11 +0300

    feat(cube): add SolvedCubeActivity

---
+++ app/src/main/java/com/example/rubiksolver/ui/cube/SolvedCubeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.rubiksolver.ui.cube
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.rubiksolver.R
+
+class SolvedCubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_solved_cube)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
