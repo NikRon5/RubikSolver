acda966e2697ef2e107f37bf30c9fe23dd2bc7a8
Nick <ronilovo2@gmail.com>
2025-05-17 16:27:36 +0300

    feat(cube): function to solve a cube

---
+++ app/src/main/java/com/example/rubiksolver/ui/cube/solver/cube_solver.py
@@ -0,0 +1,7 @@
+from twophase import solve
+
+def solve_cube(cube_string):
+    solution = solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
+    #solution = solve(cube_string)
+
+    return solution
